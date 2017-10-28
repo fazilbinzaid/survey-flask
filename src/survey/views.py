0d4a59d08c16e3a8b66775e2017557c715a85db3
fazilbinzaid <fazilibnzaid@gmail.com>
2017-10-28 21:57:12 +0530

    first view flask app

--- src/survey/views.py
+++ src/survey/views.py
@@ -1,2 +1,14 @@
+from flask import jsonify
+
 # custom imports
 from src.app import app
+from .models import *
+
+
+@app.route("/api/questions/")
+def list_questions():
+    """
+    View for listing all the questions.
+    """
+    questions = Question.query.all()
+    return jsonify(questions)
